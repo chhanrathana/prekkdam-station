f962fa46c560a41a5715584564a3be2b1559026c
Chhan Rathana <chhrathana@gmail.com>
2023-06-20 23:58:52 +0700

    layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,57 @@
 @extends('auth.auth-app')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card-group">
-            <div class="card p-4">
-                <div class="card-body">
-                    <h3 class="font-weight-bold text-center text-nowrap">ស្ថានីយ៍ឥន្ទនៈ SAVIMEX R5</h3>
-                    <br/>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <span class="material-icons-outlined">account_circle</span>                                
-                                </span>
-                            </div>
-                            <input class="form-control" type="text" name="email" value="test"  required autofocus>
+<div class="justify-content-center">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-sm-12"> 
+            <div class="card-group">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <div class="text-center d-block d-sm-none">
+                            <img class="img-fluid mx-auto rounded mb-2" style="max-height: 100px" src="{{ asset('img/brand.png') }}" alt="">
                         </div>
-                        <div class="input-group mb-4">
-                            <div class="input-group-prepend">
-                            <span class="input-group-text">
-                                <span class="material-icons-outlined">lock</span>
-                            </span>
+                        <h3 class="font-weight-bold text-center mb-4 text-nowrap">{{ __('form.system_name') }}</h3>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <span class="material-icons-outlined">account_circle</span>
+                                        </span>
+                                    </div>
+                                    <input class="form-control" type="text" placeholder="{{ __('form.account') }}" name="email" value="test"  required autofocus>
+                                </div>
                             </div>
-                            <input class="form-control" type="password" name="password" required value="FA4257->tPHZBW<P">
-                        </div>
-                        <div class="input-group mb-4 text-center">
-                            <button class="btn btn-primary" type="submit">
-                                {{-- <i class="fas fa-sign-in"></i> --}}
-                                ចូលប្រព័ន្ធ
-                            </button>
-                        </div>                        
-                    </form>
-                </div>
-                @if ($errors->any())
-                <div class="text-danger text-center"><strong>{{ $errors->first() }}</strong></div>
-                @endif
-            </div>        
-            <div class="card text-white bg-light py-5 d-md-down-none">
-                <div class="card-body text-center">
-                    <img class="img-fluid mx-auto" src="{{ asset('img/brand.png') }}" alt="">
+                            
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <span class="material-icons-outlined">lock</span>
+                                        </span>
+                                    </div>
+                                    <input class="form-control" type="password" placeholder="{{ __('form.password') }}" name="password" required value="FA4257->tPHZBW<P">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group text-center">
+                                <button class="btn btn-primary" type="submit">                                            
+                                    {{ __('form.login') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    @if ($errors->any())
+                    <div class="text-danger text-center"><strong>{{ $errors->first() }}</strong></div>
+                    @endif
+                </div>        
+                <div class="card text-white bg-light py-5 d-md-down-none">
+                    <div class="card-body text-center">
+                        <img class="img-fluid mx-auto" style="max-height: 200px" src="{{ asset('img/brand.png') }}" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    
+    </div>        
 </div>
 @stop
