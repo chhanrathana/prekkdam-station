96fedeb7eff5a004efa066452ed7a601de09dd94
Chhan Rathana <chhrathana@gmail.com>
2023-07-06 23:57:52 +0700

    style

--- resources/views/reports/operations/sales/list.blade.php
+++ resources/views/reports/operations/sales/list.blade.php
@@ -7,15 +7,17 @@
                  <strong>{{ __('form.sale_list') }}</strong>
             </div>
             <div class="col">
-                <a class="float-right btn btn-sm btn-danger" target="_blank" href="{{ route('report.operation.sale.download', ['type' => 'PDF']).currentParamter() }}">
-                    <i class="fas fa-print"></i>
-                    <strong>{{ __('form.print') }}</strong>
-                </a>
-                
-                <a class="float-right btn btn-sm btn-warning mr-1" target="_blank" href="{{ route('report.operation.sale.download', ['type' => 'EXCEL']).currentParamter() }}">
-                    <i class="fas fa-download"></i>
-                    <strong>{{ __('form.download') }}</strong>
-                </a>
+                <div class="btn-group float-right"role="group"aria-label="Action">
+                    <a class="btn btn-sm btn-danger mr-1" target="_blank" href="{{ route('report.operation.sale.download', ['type' => 'PDF']).currentParamter() }}">
+                        <i class="fas fa-print"></i>
+                        <strong>{{ __('form.print') }}</strong>
+                    </a>
+                    
+                    <a class="btn btn-sm btn-warning mr-1" target="_blank" href="{{ route('report.operation.sale.download', ['type' => 'EXCEL']).currentParamter() }}">
+                        <i class="fas fa-download"></i>
+                        <strong>{{ __('form.download') }}</strong>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
