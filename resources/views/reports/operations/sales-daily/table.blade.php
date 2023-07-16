f0c18065cfa8daddefba820163cf7184c5bf192d
Chhan Rathana <chhrathana@gmail.com>
2023-07-16 18:41:04 +0700

    improvement

--- resources/views/reports/operations/sales-daily/table.blade.php
+++ resources/views/reports/operations/sales-daily/table.blade.php
@@ -2,13 +2,18 @@
     <thead>
         <tr>
             <th class="text-center text-nowrap" style="width: 30px">{{ __('form.no') }}</th>
-            <th class="text-center text-nowrap" style="width: 100px">{{ __('form.code') }}</th>
+            <th class="text-center text-nowrap" style="width: 60px">{{ __('form.code') }}</th>
             <th class="text-center text-nowrap" style="width: 100px">{{ __('form.oil_type') }}</th>                        
-            <th class="text-center text-nowrap" style="width: 80px">{{ __('form.shift') }}</th>
+            <th class="text-center text-nowrap" style="width: 50px">{{ __('form.shift') }}</th>
             <th class="text-center text-nowrap" style="width: 100px">{{ __('form.staff') }}</th>
-            <th class="text-center text-nowrap" style="width: 80px">{{ __('form.qty') }}</th>            
-            <th class="text-center text-nowrap" style="width: 150px">{{ __('form.price') }}</th>
-            <th class="text-center text-nowrap" style="width: 150px">{{ __('form.total_price') }}</th>
+            <th class="text-center text-nowrap" style="width: 50px">{{ __('form.qty') }}</th>   
+            
+            <th class="text-center text-nowrap"style="width: 100px">{{ __('form.cost') }}</th>
+            <th class="text-center text-nowrap" style="width: 100px">{{ __('form.total_cost') }}</th>         
+
+            <th class="text-center text-nowrap" style="width: 100px">{{ __('form.price') }}</th>
+            <th class="text-center text-nowrap" style="width: 100px">{{ __('form.total_price') }}</th>
+            <th class="text-center text-nowrap" style="width: 100px">{{ __('form.total_revenue') }}</th>
         </tr>
     </thead>
     <tbody>
@@ -31,17 +36,22 @@
                 <td class="text-left text-nowrap">{{ $record->purchase->type->name_kh??''}}</td>                            
                 <td class="text-left text-nowrap">{{ $record->shift->name_kh??''}}</td>
                 <td class="text-left text-nowrap">{{ $record->staff->name_kh??''}}</td>                
-
                 <td class="text-right text-nowrap">{{ number_format($record->qty,2) }} {{ $record->unit }}</td>                
+                <td class="text-right text-nowrap">{{ number_format($record->cost,2) }} {{ $record->currency }}</td>
+                <td class="text-right text-nowrap">{{ number_format($record->total_cost,2) }} {{ $record->currency }}</td>
                 <td class="text-right text-nowrap">{{ number_format($record->price,2) }} {{ $record->currency }}</td>
                 <td class="text-right text-nowrap">{{ number_format($record->total_price,2) }} {{ $record->currency }}</td>                
+                <td class="text-right text-nowrap">{{ number_format($record->total_revenue,2) }} {{ $record->currency }}</td>
             </tr>
         @endforeach
         <tr class="text-bold">
             <td colspan="5" class="text-center"> {{ __('form.total') }}</td>
             <td class="text-right text-nowrap">{{ number_format($totalQty,2) }} L</td>               
             <td class="text-center text-nowrap">-</td>
+            <td class="text-right text-nowrap">{{ number_format($totalCost,2) }} KHR</td>
+            <td class="text-center text-nowrap">-</td>
             <td class="text-right text-nowrap">{{ number_format($totalPrice,2) }} KHR</td>
+            <td class="text-right text-nowrap">{{ number_format($totalRevenue,2) }} KHR</td>
         </tr>
         </tbody>
 </table>  
